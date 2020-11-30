b0e2ca4c67130ced7fc161ce01badcdc79c81633
Anna Golikova <70162779+agolikova@users.noreply.github.com>
2020-11-30 14:15:37 +0000

    Update DNA.py

--- Code/DNA.py
+++ Code/DNA.py
@@ -1,3 +1,7 @@
+#COUNTING DNA NUCLEOTIDES 
+#Given: DNA string s of length at most 1000 nt.
+#Return: Four integers (separated by spaces) counting respective number of times that symbols 'A', 'C', 'G', and 'T' occur in s.
+
 myfile = open('../Files/rosalind_bio_DNA.txt', 'r')
 
 for line in myfile:
