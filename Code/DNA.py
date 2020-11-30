1cb31a3dcf5b6e0134e6e02804b865cceda73510
Anna Golikova <70162779+agolikova@users.noreply.github.com>
2020-11-30 14:22:38 +0000

    Update DNA.py

--- Code/DNA.py
+++ Code/DNA.py
@@ -2,15 +2,18 @@
 #Given: DNA string s of length at most 1000 nt.
 #Return: Four integers (separated by spaces) counting respective number of times that symbols 'A', 'C', 'G', and 'T' occur in s.
 
+def count_nt(file):
+    """Return number of times that each nt appears in given DNA seq"""
+    for line in myfile:
+        for word in line.split():
+            A = word.count('A')
+            G = word.count('G')
+            C = word.count('C')
+            T = word.count('T')
+
+    print(A, C, G, T)
+
 myfile = open('../Files/rosalind_bio_DNA.txt', 'r')
-
-for line in myfile:
-    for word in line.split():
-        A = word.count('A')
-        G = word.count('G')
-        C = word.count('C')
-        T = word.count('T')
-
-print(A, C, G, T)
+count_nt(myfile)
 
 myfile.close()
