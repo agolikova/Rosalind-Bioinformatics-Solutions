b96aed3728b3a21244db3075591bb654691059e0
Anna Golikova <70162779+agolikova@users.noreply.github.com>
2020-11-30 15:26:14 +0000

    Update and rename DNA.py to DNA_Counting Nucleotides.py

--- Code/DNA_Counting Nucleotides.py
+++ Code/DNA_Counting Nucleotides.py
@@ -1,4 +1,3 @@
-#COUNTING DNA NUCLEOTIDES 
 #Given: DNA string s of length at most 1000 nt.
 #Return: Four integers (separated by spaces) counting respective number of times that symbols 'A', 'C', 'G', and 'T' occur in s.
 
