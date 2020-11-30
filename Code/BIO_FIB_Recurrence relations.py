22a429649cf390c836e2c698c8107eb2989bab90
Anna Golikova <70162779+agolikova@users.noreply.github.com>
2020-11-30 15:16:26 +0000

    Add files via upload

---
+++ Code/BIO_FIB_Recurrence relations.py
@@ -0,0 +1,16 @@
+#RABBITS AND RECURRENCE RELATIONS
+#Given: Positive integers n≤40 and  k≤5
+#Return: Total number of rabbit pairs that will be present after n months
+#Begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k rabbit pairs
+
+def rabbitcalculator(n, k):
+    """Returns total number of rabbit pairs that will be present after n months"""
+
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return rabbitcalculator(n - 1, k) + k * rabbitcalculator(n - 2, k)
+
+print(rabbitcalculator(34, 2))
