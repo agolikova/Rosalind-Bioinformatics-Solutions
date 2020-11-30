7f73916c123ea4803338128e5b6412508486094c
Anna Golikova <70162779+agolikova@users.noreply.github.com>
2020-11-30 14:47:38 +0000

    Create RNA.py

---
+++ Code/RNA.py
@@ -0,0 +1,40 @@
+#TRANSCRIBING DNA INTO RNA
+#Given: DNA string t having length at most 1000 nt
+#Return: Transcribed RNA string of t
+
+myfile = open('../Files/rosalind_bio_RNA.txt', 'r')
+
+#OPTION 1
+from Bio.Seq import Seq
+
+def transcription(file):
+    dna = Seq(file.read())
+    rna = dna.transcribe()
+    return rna
+
+print(transcription(myfile))
+
+#OPTION 2
+myfile = myfile.read()
+
+def transcription(file):
+    return file.replace("T", "U")
+
+print(transcription(myfile))
+
+#OPTION 3
+myfile = myfile.read()
+
+finalRNA = ""
+
+def transcription(file):
+    finalRNA = ""
+
+    for n in myfile:
+        if n == "T":
+            n = "U"
+        finalRNA = finalRNA + n
+
+    return finalRNA
+
+print(transcription(myfile))
