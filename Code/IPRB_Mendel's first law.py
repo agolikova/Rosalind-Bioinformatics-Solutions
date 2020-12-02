58c4b4f2a072871d516c986e360224590f08fc37
Anna Golikova <70162779+agolikova@users.noreply.github.com>
2020-12-02 17:51:32 +0000

    Update IPRB_Mendel's first law.py

--- Code/IPRB_Mendel's first law.py
+++ Code/IPRB_Mendel's first law.py
@@ -1,7 +1,6 @@
 #Given: Three positive integers k, m, and n, representing a population containing k+m+n organisms
 #k individuals = homozygous dominant for a factor, m = heterozygous, n = homozygous recessive
-#Return: Probability that two randomly selected mating organisms will produce individual possessing a dominant allele
-#...(and thus displaying the dominant phenotype). Assume that any two organisms can mate
+#Return: Probability that two randomly selected mating organisms will produce individual possessing a dominant allele (and thus displaying dominant phenotype). Assume that any two organisms can mate
 
 def calc_prob(k, m, n):
     """Return  probability that two randomly selected mating organisms will produce
