b4e3eca9d4127475e0f3e0e2dc96f79b7ec7d02c
Anna Golikova <70162779+agolikova@users.noreply.github.com>
2021-01-11 17:32:56 +0000

    Update CUNR_Counting Unrooted Binary Trees.py

--- Code/CUNR_Counting Unrooted Binary Trees.py
+++ Code/CUNR_Counting Unrooted Binary Trees.py
@@ -2,16 +2,19 @@
 #Return: Value of b(n) modulo 1,000,000. b(n) denotes total number of distinct unrooted binary trees having n labeled leaves
 #NB number of distinct unrooted binary trees = 2n - 5!! (double factorial)
 
-def count_unrooted_binary_trees(n):
-    """Returns total number of distinct unrooted binary trees having n labeled leaves, modulo 1,000,000"""
+def double_factorial(n):
+    """Returns double factorial !! of n"""
 
     if n == 0 or n == 1:
         return 1
     else:
-        return n * count_unrooted_binary_trees(n - 2)
+        return n * double_factorial(n - 2)
+
+def count_unrooted_binary_trees(n):
+    """Returns total number of distinct unrooted binary trees having n labeled leaves, modulo 1,000,000"""
+
+    output = double_factorial(2 * n - 5) % 1000000
+    return output
 
 n = 915
-
-output = count_unrooted_binary_trees(2 * n - 5) % 1000000
-
-print(output)
+print(count_unrooted_binary_trees(n))
