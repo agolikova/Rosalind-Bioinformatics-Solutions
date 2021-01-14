58f81d1c6780b735063ee32ea284b2defbf77ec0
Anna Golikova <70162779+agolikova@users.noreply.github.com>
2021-01-14 15:20:38 +0000

    Add files via upload

---
+++ Code/LCSQ_Finding a Shared Spliced Motif.py
@@ -0,0 +1,28 @@
+#Given: Two DNA strings s and t in FASTA format
+#Return: A longest common subsequence of s and t (longest non-contiguous collection of nt's shared by both seqs)
+
+from Bio import SeqIO
+
+def longest_common_subsequence(seq1, seq2):
+    """Prints longest common subsequence of 2 given seqs"""
+
+    cur = [''] * (len(seq2) + 1)
+
+    for x in seq1:
+        last, cur = cur, ['']
+
+        for i, y in enumerate(seq2):
+            cur.append(last[i] + x if x == y else max(last[i+1], cur[-1], key=len))
+
+    print(cur[-1])
+
+with open('../Files/rosalind_bio_LCSQ2.txt', 'r') as myfile:
+    sequences = []
+
+    for record in SeqIO.parse(myfile, "fasta"):
+        sequences.append(str(record.seq))
+
+    s = sequences[0]
+    t = sequences[1]
+
+longest_common_subsequence(s, t)
