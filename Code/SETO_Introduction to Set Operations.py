d390e091f90b3d0397725f1e2404a166d2d667d6
Anna Golikova <70162779+agolikova@users.noreply.github.com>
2021-01-23 15:32:17 +0000

    Add files via upload

---
+++ Code/SETO_Introduction to Set Operations.py
@@ -0,0 +1,31 @@
+#Given: Positive integer n (n≤20,000) and two subsets A and B of {1,2,…,n}
+#Return: Six sets: A∪B, A∩B, A−B, B−A, A^c, and B^c (where set complements are taken with respect to {1,2,…,n})
+
+def union(a, b):
+    #return a | b
+    return A.union(B)
+
+def intersection(a, b):
+    #return a & b
+    return A.intersection(B)
+
+def set_difference(a, b):
+    return a - b
+
+def set_complement(u, a):
+    return u - a
+
+with open('../Files/rosalind_bio_SETO.txt', 'r') as myfile:
+    mylines = myfile.read().splitlines()
+
+    n = int(mylines[0].rstrip())
+    U = set(range(1, n + 1))
+    A = set([int(x) for x in mylines[1].strip('{}').split(',')])
+    B = set([int(x) for x in mylines[2].strip('{}').split(',')])
+
+print(union(A, B))
+print(intersection(A, B))
+print(set_difference(A, B))
+print(set_difference(B, A))
+print(set_complement(U, A))
+print(set_complement(U, B))
