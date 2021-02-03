b892e6e3d9f6c4db34bff968d3a9489d1a5487a7
Anna Golikova <70162779+agolikova@users.noreply.github.com>
2021-02-03 16:39:10 +0000

    Update TRIE_Introduction to Pattern Matching.py

--- Code/TRIE_Introduction to Pattern Matching.py
+++ Code/TRIE_Introduction to Pattern Matching.py
@@ -1,7 +1,7 @@
 #Given: List of at most 100 DNA strings, none of which is a prefix of another
 #Return: Adjacency list corresponding to trie T for these patterns, in following format:
 #If T has n nodes, first label root with 1 then label remaining nodes with integers 2 through n in any order
-#Each edge of adjacency list of T will be encoded by a triple containing integer representing edge's parent node followed by integer representing edge's child node, and finally symbol labeling edge
+#Each edge of adjacency list of T will be encoded by triple containing: integer representing edge's parent node, integer representing edge's child node, symbol labelling edge
 
 import itertools
 from collections import defaultdict
@@ -32,7 +32,7 @@ class Trie:
 
 T = Trie()
 
-with open('../Files/rosalind_bio_TRIE2.txt', 'r') as myfile:
+with open('../Files/rosalind_bio_TRIE.txt', 'r') as myfile:
     for s in myfile.read().split('\n'):
         T.add_child(s)
 
